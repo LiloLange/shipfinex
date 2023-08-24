95a36efbd1e0bd4ccc7efb53f56f73a81c069b05
sage <sage191709@gmail.com>
2023-08-24 07:56:47 +0000

    21th

--- dist/routes/user.js
+++ dist/routes/user.js
@@ -64,7 +64,13 @@ exports.userRoute = [
                 console.log(baseUrl);
                 const content = `<div style="background-color: #f2f2f2; padding: 20px; border-radius: 10px;"><h1 style="font-size: 36px; color: #333; margin-bottom: 20px;">Hello</h1><p style="font-size: 18px; color: #666; margin-bottom: 20px;">Welcome To ShipFinex Homepage</p><p style="font-size: 18px; color: #666; margin-bottom: 40px;">This is your email verification link. Please click the button below to verify your email:</p><a href="${baseUrl}/api/v1/user/verify-email/${token}" style="background-color: #4CAF50; color: white; padding: 10px 20px; text-decoration: none; border-radius: 10px; font-size: 18px;">Verify Email</a></div>`;
                 (0, sendMail_1.default)(result.email, content);
-                return response.response(result).code(201);
+                return response
+                    .response({
+                    email: result.email,
+                    firstName: result.firstName,
+                    lastName: result.lastName,
+                })
+                    .code(201);
                 // return token;
             }
             catch (error) {
